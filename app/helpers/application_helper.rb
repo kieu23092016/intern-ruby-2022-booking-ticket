2169ed23237508bf389f12027756fddb4e71b9fa
Cong Vu <vuquoccong01@gmail.com>
2022-09-09 14:45:03 +0700

    Select movie

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  include Pagy::Frontend
+
   def full_title page_title = " "
     base_title = t "text.intern_ruby"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
